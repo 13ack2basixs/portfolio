84efca08c862ee1a50a4808f01879d649272c987
Hong Wei Tai <e1138406@u.nus.edu>
2025-04-01 03:59:25 +0800

    Add name and changed font

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import './App.css'
+import '@fontsource-variable/fira-code';
+import { FirstName, LastName } from './components/Name/Name';
 import NavBar from './components/NavBar/NavBar';
-import Section from './components/Section/Section';
 
 function App() {
 
   return (
     <div>
+      <div className='name-container'>
+        <LastName />
+        <FirstName />
+      </div>
       <NavBar />
 
-      <Section id="about">about</Section>
-      <Section id="skills">skills</Section>
-      <Section id="projects">projects</Section>
-      <Section id="experience">experience</Section>
-      <Section id="contact">contact</Section>
     </div>
   )
 }
---
+++ src/components/Name/Name.jsx
@@ -0,0 +1,14 @@
+import './Name.css'
+
+export const FirstName = () => {
+    return (
+        <p className='first-name'>Hong Wei</p>
+    )
+}
+
+export const LastName = () => {
+    return (
+        <p className='last-name'>Tai</p>
+    )
+}
+
