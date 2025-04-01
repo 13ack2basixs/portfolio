d7220cc14092436f6a7bde31946322412d9dc8f8
Hong Wei Tai <e1138406@u.nus.edu>
2025-04-01 10:17:56 +0800

    Add about section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,14 @@
+import './About.css'
+
+const about = () => {
+    return (
+        <div className='about-container'> 
+            <h2 id='about'>About Me</h2>
+            <div className='about-para-container'>
+                <p>I am Hong Wei, a Business Analytics undergraduate, I love coding software to help people and dealing with complex data!</p>
+            </div>    
+        </div>
+    )
+}
+
+export default about;
