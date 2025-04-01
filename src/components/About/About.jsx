c0ed3fdd08d9fd5e11683fa650bc709804593757
Hong Wei Tai <e1138406@u.nus.edu>
2025-04-01 14:59:01 +0800

    Add NUS hyperlink

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ const about = () => {
         <div className='about-container'> 
             <h2 id='about'>About Me</h2>
             <div className='about-para-container'>
-                <p>I am Hong Wei, a Business Analytics undergraduate, I love coding software to help people and dealing with complex data!</p>
+                <p>Welcome to my website! I am Hong Wei, a Business Analytics undergraduate at <a className='nus-link' href='https://www.nus.edu.sg/' target='_blank'>NUS</a>. I love coding software to help people and dealing with complex data!</p>
             </div>    
         </div>
     )
