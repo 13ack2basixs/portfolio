dcd8ae92ada2392dc1da38ffc3690d78ae7e84b9
Hong Wei Tai <e1138406@u.nus.edu>
2025-04-03 09:13:53 +0800

    Add contact me description

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,6 +5,7 @@ const Contact = () => {
     return (
         <div className='contact-container'>
             <h2 id='contact'>Contact</h2>
+            <p>Feel free to send me a message if you have a project idea that you think we could work together!</p>
             <ContactForm />
         </div>
     )
