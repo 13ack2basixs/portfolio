e313c756ba5025c662560748fa569524523e0eeb
Hong Wei Tai <e1138406@u.nus.edu>
2025-04-03 09:15:32 +0800

    Increase font size for intro

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ const about = () => {
         <div className='about-container'> 
             <h2 id='about'>About Me</h2>
             <div className='about-para-container'>
-                <p>Welcome to my website! I am Hong Wei, a Business Analytics undergraduate at <a className='nus-link' href='https://www.nus.edu.sg/' target='_blank'>NUS</a>. I love coding software to help people and dealing with complex data!</p>
+                <p className='intro'>Welcome to my website! I am Hong Wei, a Business Analytics undergraduate at <a className='nus-link' href='https://www.nus.edu.sg/' target='_blank'>NUS</a>. I love coding software to help people and dealing with complex data!</p>
             </div>    
         </div>
     )
