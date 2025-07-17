3a2f4a9b71a7ca7a50e35cf9c2290074753da836
Hong Wei Tai <e1138406@u.nus.edu>
2025-07-18 01:19:14 +0800

    Add urls for tech stack icons

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -37,44 +37,44 @@ const skills = () => {
 
                 <p className='skills-subtitle'>languages</p>
                 <div className='languages-icon-container'>
-                    <img src={pythonSVG} alt='python' className='scale-up-sm-lang'/>
-                    <img src={javaSVG} alt='java'/>
-                    <img src={javascriptSVG} alt='javascript'/>
-                    <img src={typescriptSVG} alt='typescript'/>
-                    <img src={mysqlSVG} alt='mysql' className='scale-up-sm-lang'/>
-                    <img src={postgresqlSVG} alt='postgresql'/>
-                    <img src={htmlSVG} alt='html'/>
-                    <img src={cssSVG} alt='css'/>
-                    <img src={rSVG} alt='r'/>
+                    <a href='https://www.python.org/' target='_blank'><img src={pythonSVG} alt='python' className='scale-up-sm-lang'/></a>
+                    <a href='https://www.java.com/en/' target='_blank'><img src={javaSVG} alt='java'/></a>
+                    <a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript' target='_blank'><img src={javascriptSVG} alt='javascript'/></a>
+                    <a href='https://www.typescriptlang.org/' target='_blank'><img src={typescriptSVG} alt='typescript'/></a>
+                    <a href='https://www.mysql.com/' target='_blank'><img src={mysqlSVG} alt='mysql' className='scale-up-sm-lang'/></a>
+                    <a href='https://www.postgresql.org/' target='_blank'><img src={postgresqlSVG} alt='postgresql'/></a>
+                    <a href='https://developer.mozilla.org/en-US/docs/Web/HTML' target='_blank'><img src={htmlSVG} alt='html'/></a>
+                    <a href='https://developer.mozilla.org/en-US/docs/Web/CSS' target='_blank'><img src={cssSVG} alt='css'/></a>
+                    <a href='https://www.r-project.org/' target='_blank'><img src={rSVG} alt='r'/></a>
                 </div>
 
                 <p className='skills-subtitle'>frameworks</p>
                 <div className='frameworks-icon-container'>
-                    <img src={reactSVG} alt='react' className='scale-up-sm-fw'/>
-                    <img src={nodejsSVG} alt='nodejs'/>
-                    <img src={vuejsSVG} alt='vuejs'/>
-                    <img src={djangoSVG} alt='django'/>
-                    <img src={flaskSVG} alt='flask'/>
-                    <img src={streamlitSVG} alt='streamlit'/>
-                    <img src={umijsSVG} alt='umijs'/>
-                    <img src={tailwindSVG} alt='tailwind'/>
+                    <a href='https://react.dev/' target='_blank'><img src={reactSVG} alt='react' className='scale-up-sm-fw'/></a>
+                    <a href='https://nodejs.org/en' target='_blank'><img src={nodejsSVG} alt='nodejs'/></a>
+                    <a href='https://vuejs.org/' target='_blank'><img src={vuejsSVG} alt='vuejs'/></a>
+                    <a href='https://www.djangoproject.com/' target='_blank'><img src={djangoSVG} alt='django'/></a>
+                    <a href='https://flask.palletsprojects.com/en/stable/' target='_blank'><img src={flaskSVG} alt='flask'/></a>
+                    <a href='https://streamlit.io/' target='_blank'><img src={streamlitSVG} alt='streamlit'/></a>
+                    <a href='https://umijs.org/en-US' target='_blank'><img src={umijsSVG} alt='umijs'/></a>
+                    <a href='https://tailwindcss.com/' target='_blank'><img src={tailwindSVG} alt='tailwind'/></a>
                 </div>
 
                 <p className='skills-subtitle'>tools</p>
                 <div className='tools-icon-container'>
-                    <img src={gitSVG} alt='git' className='scale-up-sm-tool'/>
-                    <img src={dockerSVG} alt='docker' className='scale-up-sm-tool'/>
-                    <img src={tableauSVG} alt='tableau'/>
-                    <img src={mongodbSVG} alt='mongodb' className='scale-up-sm-tool'/>
-                    <img src={firebaseSVG} alt='firebase'/>
-                    <img src={awsSVG} alt='aws'/>
+                    <a href='https://git-scm.com/' target='_blank'><img src={gitSVG} alt='git' className='scale-up-sm-tool'/></a>
+                    <a href='https://www.docker.com/' target='_blank'><img src={dockerSVG} alt='docker' className='scale-up-sm-tool'/></a>
+                    <a href='https://www.tableau.com/' target='_blank'><img src={tableauSVG} alt='tableau'/></a>
+                    <a href='https://www.mongodb.com/' target='_blank'><img src={mongodbSVG} alt='mongodb' className='scale-up-sm-tool'/></a>
+                    <a href='https://firebase.google.com/' target='_blank'><img src={firebaseSVG} alt='firebase'/></a>
+                    <a href='https://aws.amazon.com/' target='_blank'><img src={awsSVG} alt='aws'/></a>
                 </div>
 
                 <p className='skills-subtitle'>libraries</p>
                 <div className='libraries-icon-container'>
-                    <img src={pandasSVG} alt='pandas'/>
-                    <img src={numpySVG} alt='numpy'/>
-                    <img src={chartjsSVG} alt='chartjs'/>
+                    <a href='https://pandas.pydata.org/' target='_blank'><img src={pandasSVG} alt='pandas'/></a>
+                    <a href='https://numpy.org/' target='_blank'><img src={numpySVG} alt='numpy'/></a>
+                    <a href='https://www.chartjs.org/' target='_blank'><img src={chartjsSVG} alt='chartjs'/></a>
                 </div>
             </div>
         </div>
