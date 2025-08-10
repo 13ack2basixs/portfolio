9a5be6fab7f0b38d3ab496cc245a910191c78e11
Hong Wei Tai <e1138406@u.nus.edu>
2025-08-10 23:39:12 +0800

    refactor: change intro

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ const about = () => {
         <div className='about-container'> 
             <h2 id='about'>About Me</h2>
             <div className='about-para-container'>
-                <p className='intro'>Welcome to my website! I am Hong Wei, a Business Analytics undergraduate at NUS. I love coding software to help people and dealing with complex data!</p>
+                <p className='intro'>Welcome to my website, my name is Hong Wei! I am a penultimate Business Analytics undergraduate at NUS. My passion lies in ML, Web3, creating social good and I love coding software that makes a positive impact on communities.</p>
             </div>    
         </div>
     )
