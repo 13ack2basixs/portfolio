5b37c97ee68db27b89e530bee0661fcf48915d5a
hongggweiii <103654486+13ack2basixs@users.noreply.github.com>
2025-10-28 22:52:53 +0800

    fix: import erro

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import '@fontsource-variable/fira-code';
 import { FirstName, LastName } from './components/Name/Name';
 import NavBar from './components/NavBar/NavBar';
 import About from './components/About/About';
